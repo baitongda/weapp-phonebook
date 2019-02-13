2137523352eac6c3051757ce4fbcb0fd17318944
heming6666 <1040023934@qq.com>
2019-02-13 21:29:19 +0800

    修正首页分类显示不全的问题

--- cloudfunctions/getuserlist/index.js
+++ cloudfunctions/getuserlist/index.js
@@ -11,7 +11,7 @@ exports.main = async (event, context) => {
   const countResult = await db.collection('user').count()
   const total = countResult.total
   // 计算需分几次取
-  const batchTimes =(total / 100)
+  const batchTimes =Math.ceil(total / 100)
   // 承载所有读操作的 promise 的数组
   const tasks = []
   if (event.categoryId!=0){
